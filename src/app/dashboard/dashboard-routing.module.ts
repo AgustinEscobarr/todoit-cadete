bae6b87f98d3ec83e2de5d1eef20831b4fe1a5ed
AgustinEscobarr <agustinescobaring@gmail.com>
2021-12-06 23:21:18 -0300

    armado proyecto cadete

---
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard.component';
+import { HistoryComponent } from './pages/history/history.component';
+import { HomeComponent } from './pages/home/home.component';
+import { TravelsComponent } from './pages/travels/travels.component';
+
+const routes: Routes = [
+  { 
+    path: '', 
+    component: DashboardComponent 
+  },
+  {
+    path:'travels',
+    component:TravelsComponent
+  },
+  {
+    path:'history',
+    component:HistoryComponent
+  },
+  {
+    path:'home',
+    component:HomeComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class DashboardRoutingModule { }
